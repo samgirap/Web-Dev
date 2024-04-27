0b88bde8cdb4b230d11cf5d6d22aa692ab7d02ea
samgirap <labuser@ip-172-31-4-33.ap-south-1.compute.internal>
2024-04-27 06:37:41 +0000

    Version 2

--- script.java
+++ script.java
@@ -11,3 +11,15 @@ class HelloWorld {
     }
 
 }
+class Main {
+
+  public static void main(String[] args) {
+
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
